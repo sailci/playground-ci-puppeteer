d65c79070a20774c0b08df4e490319f6f7655f05
entrptaher@gmail.com <entrptaher@gmail.com>
2018-10-28 09:31:11 +0600

    xvfb test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const example = require('./index');
 
 describe('Puppeteer', () => {
   before(async function () {
-    this.browser = await puppeteer.launch({headless: false});
+    this.browser = await puppeteer.launch({ headless: false });
     this.page = await this.browser.newPage();
   });
 
